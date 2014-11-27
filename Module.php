7039ac626fb982b94be46d20ec68d96fc9fcc341
Pavel Bariev <bariew@yandex.ru>
2014-11-27 11:21:08 +0400

    removed params file

--- Module.php
+++ Module.php
@@ -4,5 +4,12 @@ namespace bariew\configModule;
 
 class Module extends \yii\base\Module
 {
-
+    public $params =  [
+        'menu'  => [
+            'label'    => 'Settings',
+            'items' => [
+                ['label' => 'App config', 'url' => ['/config/item/update', 'name' => 'Main']]
+            ]
+        ]
+    ];
 }
--- params.php
+++
@@ -1,9 +0,0 @@
-<?php  
-return [
-    'menu'  => [
-        'label'    => 'Settings',
-        'items' => [
-            ['label' => 'App config', 'url' => ['/config/item/update', 'name' => 'Main']]
-        ]
-    ]
-];
